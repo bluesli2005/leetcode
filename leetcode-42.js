f49661d3effa59ceea55f344d96461bf8b317a28
INTRA\ts-wenbiao.li <ts-wenbiao.li@rakuten.com>
2019-03-27 12:45:54 +0900

    leetcode-42,leetcode-11

---
+++ leetcode-42.js
@@ -0,0 +1,16 @@
+var trap = function (height) {
+    let i = 0 , j = height.length - 1 , res = 0,
+        leftMax  = height[i], rightMax = height[j];
+    while (i < j ) {
+        leftMax = Math.max(leftMax, height[i]);
+        rightMax = Math.max(rightMax, height[j]);
+        if(leftMax > rightMax) {
+            res += rightMax - height[j];
+            j--
+        } else {
+            res += leftMax - height[i];
+            i++
+        }
+    }
+    return res;
+}
