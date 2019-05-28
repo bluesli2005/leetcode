fd479a2aff9146f77110460e3644483938a2ff0f
INTRA\ts-wenbiao.li <ts-wenbiao.li@rakuten.com>
2019-05-28 11:33:29 +0900

    add leetcode 39

---
+++ leetcode-39.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */ 
+ 
+ var combinationSum = function(candidates, target) {
+    let result=[],temp=[];
+    let find =function(target,temp,index){
+        for(let i=index;i>=0;i--){
+            if(candidates[i]>target){
+                continue; 
+            }else if(candidates[i]===target){
+                result.push(temp.concat([candidates[i]]));
+            }else{ 
+                find(target-candidates[i],temp.concat([candidates[i]]),i);
+            }
+        }
+    }
+
+    find(target,temp,candidates.length-1);
+    return result;
+ }
