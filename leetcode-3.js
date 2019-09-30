922577b87c2cc510f0020ead33f90e31944c1837
liwenbiao <lwbyx2000@hotmail.com>
2019-09-30 17:25:19 +0900

    add leetcode 3

---
+++ leetcode-3.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+   var left = 0; 
+   const map = {}
+   return s.split('').reduce((max, v, i) => { 
+       left = map[v] >= left ? map[v] + 1 : left; 
+       map[v] = i; 
+       return Math.max(max, i - left + 1); 
+   }, 0);
+};
