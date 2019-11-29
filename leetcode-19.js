d3ae3367b40e1b29f824f87c0c117bfced643831
liwenbiao <lwbyx2000@hotmail.com>
2019-11-29 12:08:24 +0900

    add leetcode 19

--- leetcode-19.js
+++ leetcode-19.js
@@ -11,40 +11,28 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let start = new ListNode(null);
-    start.next = head;
-    let len = 0,
-        current = null,
-        del = null;
-    if(head === null){
-        len = 0;
-    }else{
-        current = head;
-        len = 1;
-        while(current.next){
-            len++;
-        }
+    var n1 = new ListNode();
+    var n2 = new ListNode();
+    var dummy = n2;
+    
+    n1.next = head;
+    n2.next = head;
+    
+    while(n > 0 && n1){
+        n1 = n1.next;
+        n--;
     }
-    let position = len - n;
-    if(position > -1 && position < len){
-        current = head;
-        let previous,
-            index = 0;
-        // 移除第一项
-        if(position === 0){
-            head = current.next;
-        }else{
-            while(index < position){
-                previous = current;
-                current = current.next;
-                index++;
-            }
-            del = current;
-            previous.next = current.next;
-        }
-        len--;
-        return start.next;
-    }else{
-        return null;
+    
+    if(n > 0){
+        return head;
     }
+    
+    while(n1 && n1.next){
+        n1 = n1.next;
+        n2 = n2.next;
+    }
+    
+    n2.next = n2.next.next;
+    
+    return dummy.next;
 };
