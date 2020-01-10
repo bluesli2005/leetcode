9da704ff9fb4bdd40d9e7f0b05e998fdaba588bc
bluesky <bluesky@blueskynoMacBook-Pro.local>
2020-01-10 17:45:36 +0900

    fix

--- leetcode-3.js
+++ leetcode-3.js
@@ -6,7 +6,9 @@ var lengthOfLongestSubstring = function(s) {
    var left = 0; 
    const map = {}
    return s.split('').reduce((max, v, i) => { 
-       left = map[v] >= left ? map[v] + 1 : left; 
+       left = map[v] >= left 
+              ? map[v] + 1 
+              : left; 
        map[v] = i; 
        return Math.max(max, i - left + 1 ); 
    }, 0);
